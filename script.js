220ae4159dffa9323f40a00d4f4821bd440dcd3e
Laoise Earle <laoiseearle92@gmail.com>
2023-02-14 21:15:00 +0000

    Close menu after resizing window

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const hamIcon = document.getElementById('hamIcon');
 const menu = document.getElementById('menu');
 
+window.addEventListener('resize', e => {
+  if (e.target.innerWidth >= 700) {
+    hamIcon.src = 'images/icon-hamburger.svg';
+    menu.classList.remove('menu-open');
+    document.body.style.overflow = 'visible';
+  }
+});
+
 const openMenu = () => {
   if (menu.classList.contains('menu-open')) {
     hamIcon.src = 'images/icon-hamburger.svg';
